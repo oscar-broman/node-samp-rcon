64142f8502eed3dcb09434f5012b2b2d89efaf78
Oscar Broman <oscar.broman@gmail.com>
2013-05-17 18:03:29 +0200

    add web interface

---
+++ main.js
@@ -0,0 +1,88 @@
+(function() {
+  'use strict';
+  
+  var $console = $('.rcon-console');
+  var $output = $console.find('.output');
+  var $outputPad = $output.find('.pad');
+  var $outputPre = $output.find('pre');
+  var $prompt = $console.find('.prompt');
+  var socket = io.connect(location.protocol + '//' + location.hostname);
+  var host = null;
+  var pass = null;
+  
+  function updateOutput(appendText) {
+    if (appendText) {
+      $outputPre.append($('<span/>').text('\n' + appendText));
+    }
+    
+    var scrollHeight = $output[0].scrollHeight;
+    
+    $outputPad.css('margin-top', Math.max(-$outputPad.height(), -$outputPre.height()));
+    $output.scrollTop(
+      scrollHeight
+    );
+  }
+  
+  updateOutput();
+  
+  $prompt.on('keyup', function(e) {
+    if (e.which === 13) {
+      var val = $.trim($(this).val());
+      
+      if (val) {
+        if (!host) {
+          host = val;
+          
+          $outputPre.append($('<span/>').text(host));
+          updateOutput('Password:');
+        } else if (!pass) {
+          pass = val;
+          updateOutput('Connecting..');
+          $prompt.attr('disabled', true);
+          socket.emit('rcon-connect', {
+            host: host,
+            pass: pass
+          });
+        } else {
+          updateOutput('> ' + val);
+          socket.emit('rcon-send', val);
+        }
+        
+        $prompt.val('');
+      }
+    }
+  });
+  
+  socket.on('connect', function () {
+    $prompt.removeAttr('disabled');
+  });
+  
+  socket.on('rcon-connect', function () {
+    $prompt.removeAttr('disabled');
+    updateOutput('Connected!');
+  });
+
+  socket.on('disconnect', function () {
+    $prompt.attr('disabled', true);
+  });
+  
+  socket.on('rcon-output', function (output) {
+    updateOutput('< ' + output.replace('\n', '\n< '));
+  });
+  
+  socket.on('rcon-error', function (err) {
+    var message;
+    
+    if (typeof err === 'object') {
+      if (err.code === 'ENOTFOUND') {
+        message = 'Invalid host';
+      } else {
+        message = err.message;
+      }
+    } else {
+      message = err;
+    }
+    
+    updateOutput('Error: ' + message.replace('\n', '\nError: '));
+  });
+}());
---
+++ web-server.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+var RconConnection = require('./samp-rcon');
+
+var port = +process.argv[2] || 3000;
+
+app.listen(port);
+
+function handler(req, res) {
+  var file, type;
+  
+  if (req.url === '/') {
+    file = 'index.html';
+    type = 'text/html';
+  } else if (req.url === '/style.css') {
+    file = 'style.css';
+    type = 'text/css';
+  } else if (req.url === '/main.js') {
+    file = 'main.js';
+    type = 'application/javascript';
+  }
+  
+  if (file) {
+    fs.readFile(
+      __dirname + '/' + file,
+      function (err, data) {
+        if (err) {
+          res.writeHead(500);
+          return res.end('Error loading ' + file);
+        }
+
+        res.writeHead(200, {'Content-Type': type});
+        res.end(data);
+      }
+    );
+  } else {
+    res.writeHead(404);
+    res.end('File not found');
+  }
+}
+
+io.set('log level', 2);
+
+io.sockets.on('connection', function (socket) {
+  var rcon;
+  
+  socket
+    .on('disconnect', function() {
+      rcon = null;
+    })
+    .on('rcon-connect', function (data) {
+      rcon = new RconConnection(data.host, null, data.pass);
+    
+      rcon
+        .on('ready', function() {
+          socket.emit('rcon-connect');
+        })
+        .on('message', function(msg) {
+          socket.emit('rcon-output', msg);
+        })
+        .on('error', function(err) {
+          socket.emit('rcon-error', err);
+        });
+    })
+    .on('rcon-send', function(str) {
+      if (!rcon) {
+        socket.emit('rcon-error', {
+          message: 'Not connected',
+          code: 'NOCONN'
+        });
+      } else {
+        rcon.send(str);
+      }
+    });
+});
