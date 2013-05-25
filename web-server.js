c034e6964948cf6e88a8032671ae2df59e71c3a6
Oscar Broman <oscar.broman@gmail.com>
2013-05-26 00:17:26 +0200

    close the rcon on disconnect

--- web-server.js
+++ web-server.js
@@ -51,6 +51,8 @@ io.sockets.on('connection', function (socket) {
   
   socket
     .on('disconnect', function() {
+      rcon.close();
+      
       rcon = null;
     })
     .on('rcon-connect', function (data) {
